768ce1634d94b90a42cd28e65566c686d151e391
Jonathan Paredes <47874562+Jp283x@users.noreply.github.com>
2021-04-20 00:26:19 -0700

    + Temporary Health Bar
    + 4k Health Bar

--- Client/Assets/Scripts/PlayerManager.cs
+++ Client/Assets/Scripts/PlayerManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -9,12 +10,31 @@ public class PlayerManager : MonoBehaviour
     public float health;
     public float maxHealth;
     public SpriteRenderer model;
-    
+
+    public GameObject HealthPrefab;
+    public Image BarBorder;
+    public Image BarFilled;
+
+    public float currentValue;
+
+    // public float currentHealth;
+
+    void Start()
+    {
+        BarBorder = Instantiate(HealthPrefab, FindObjectOfType<Canvas>().transform).GetComponent<Image>();
+        BarFilled = new List<Image>(BarBorder.GetComponentsInChildren<Image>()).Find(img => img != BarBorder);
+    }
+
     public void Initialize(int _id, string _username)
     {
         id = _id;
         username = _username;
         health = maxHealth;
+    }
+
+    void Update()
+    {
+        HandleHealthBar();
     }
 
     public void SetHealth(float _health)
@@ -25,6 +45,19 @@ public class PlayerManager : MonoBehaviour
         {
             Die();
         }
+    }
+
+    private float HealthMap(float currentHealth, float healthMin, float healthMax, float barMin, float barMax)
+    {
+        return (currentHealth - healthMin) * (barMax - barMin) / (healthMax - healthMin) + barMin;
+    }
+
+    private void HandleHealthBar()
+    {
+
+        currentValue = HealthMap(health, 0, maxHealth, 0, 1);
+
+        BarFilled.fillAmount = Mathf.Lerp(BarFilled.fillAmount, currentValue, Time.deltaTime);
     }
 
     public void Die()
